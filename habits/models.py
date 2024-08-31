0692458c014af70693fd5f6cbe2446bea0eccd28
Вячеслав Лукин <bafana.slv@gmail.com>
2024-08-31 16:31:58 +0300

    Create Users.

--- habits/models.py
+++ habits/models.py
@@ -5,7 +5,7 @@ NULLABLE = {"null": True, "blank": True}
 
 
 class Habit(models.Model):
-    FREQUENCY_UNITS = [
+    UNITS_OF_TIME = [
         ("minutes", "минуты"),
         ("hours", "часы"),
         ("days", "дни"),
@@ -14,26 +14,26 @@ class Habit(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        verbose_name="Cоздатель привычки",
+        verbose_name="Обладатель привычки",
         **NULLABLE,
     )
-    place = models.CharField(max_length=200, verbose_name="Место привычки")
-    time = models.DateTimeField(verbose_name="Дата и время привычки")
+    location = models.CharField(max_length=200, verbose_name="Место привычки")
+    date_time = models.DateTimeField(verbose_name="Дата и время привычки")
     action = models.CharField(max_length=200, verbose_name="Действие")
-    is_pleasant = models.BooleanField(verbose_name="Приятная")
+    is_nice = models.BooleanField(verbose_name="Приятная")
     related_habit = models.ForeignKey(
         "self", on_delete=models.SET_NULL, **NULLABLE, verbose_name="Связанная привычка"
     )
     frequency_number = models.PositiveIntegerField(verbose_name="Количество раз")
     frequency_unit = models.CharField(
         max_length=10,
-        choices=FREQUENCY_UNITS,
+        choices=UNITS_OF_TIME,
         default="days",
         verbose_name="Единицы измерения",
     )
-    reward = models.CharField(max_length=200, verbose_name="Вознаграждение", **NULLABLE)
-    duration = models.DurationField(verbose_name="Длительность действия")
-    is_public = models.BooleanField(default=True, verbose_name="Публичная")
+    present = models.CharField(max_length=200, verbose_name="Вознаграждение", **NULLABLE)
+    complete_time = models.DurationField(verbose_name="Время на выполнение")
+    is_public = models.BooleanField(default=True, verbose_name="Публичная привычка")
 
     def __str__(self):
         return f"{self.user} - {self.action}"
