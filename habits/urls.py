ced003cf743938fd5a296551c585c6851e0ca145
Вячеслав Лукин <bafana.slv@gmail.com>
2024-09-02 16:55:50 +0300

    Create Habits.

--- habits/urls.py
+++ habits/urls.py
@@ -4,18 +4,25 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 from habits.apps import HabitsConfig
-from rest_framework.routers import SimpleRouter
-from habits.views import HabitViewSet, PublicHabitListAPIView
-
+from habits.views import (
+    HabitListAPIView,
+    HabitCreateAPIView,
+    HabitRetrieveAPIView,
+    HabitUpdateAPIView,
+    HabitDestroyAPIView,
+    HomeListAPIView,
+)
 
 app_name = HabitsConfig.name
 
-router = SimpleRouter()
-router.register("", HabitViewSet, basename="habits")
-
 urlpatterns = [
-    path("public/", PublicHabitListAPIView.as_view(), name="public"),
-] + router.urls
+    path("", HomeListAPIView.as_view(), name="home-habits"),
+    path("habits/", HabitListAPIView.as_view(), name="habits"),
+    path("create/", HabitCreateAPIView.as_view(), name="create"),
+    path("<int:pk>/", HabitRetrieveAPIView.as_view(), name="habit-view"),
+    path("update/<int:pk>/", HabitUpdateAPIView.as_view(), name="habit-update"),
+    path("delete/<int:pk>/", HabitDestroyAPIView.as_view(), name="habit-delete"),
+]
 
 schema_view = get_schema_view(
     openapi.Info(
--- habits/views.py
+++ habits/views.py
@@ -1,38 +1,99 @@
-from rest_framework import viewsets, generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from habits.models import Habit
 from habits.paginations import HabitPaginator
-from users.permissions import IsOwner
 from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
+# from habits.services import send_telegram_message
+from rest_framework.response import Response
 
 
-class HabitViewSet(viewsets.ModelViewSet):
+class HomeListAPIView(generics.ListAPIView):
+    """
+    Класс вывода привычек с флагом публичные.
+    """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+    permission_classes = (AllowAny,)
+    pagination_class = HabitPaginator
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """
+    Класс просмотра привычек пользователя.
+    """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
+
     def get_queryset(self):
         return Habit.objects.filter(user=self.request.user)
 
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """
+    Класс создания привычки.
+    """
+
     serializer_class = HabitSerializer
-    pagination_class = HabitPaginator
+    queryset = Habit.objects.all()
 
     def perform_create(self, serializer):
-        new_habit = serializer.save()
-        new_habit.user = self.request.user
-        new_habit.save()
+        """
+        Привязка привычки к пользователю.
+        :param serializer:
+        :return:
+        """
+        habit = serializer.save()
+        habit.user = self.request.user
+        habit.save()
 
-    def get_permissions(self):
-        print(self.action)
-        if self.action == "create":
-            self.permission_classes = (IsAuthenticated,)
-        elif self.action in ["update", "retrieve", "list"]:
-            self.permission_classes = (IsOwner,)
-        elif self.action == "destroy":
-            self.permission_classes = (
-                IsAuthenticated, IsOwner,
-            )
-        return super().get_permissions()
+    # def post(self, request, *args, **kwargs):
+    #     user = self.request.user
+    #     message = "привычка создана"
+    #     if user.tg_nick:
+    #         send_telegram_message(user.tg_nick, message)
+    #
+    #     return Response({"message": message})
 
 
-class PublicHabitListAPIView(generics.ListAPIView):
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    Класс просмотра подробностей привычки.
+    """
+
     serializer_class = HabitSerializer
-    queryset = Habit.objects.filter(is_public=True)
-    pagination_class = HabitPaginator
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """
+    Класс изменения привычки.
+    """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+    def perform_create(self, serializer):
+        """
+        Привязка привычки к пользователю.
+        :param serializer:
+        :return:
+        """
+        habit = serializer.save()
+        habit.user = self.request.user
+        habit.save()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """
+    Класс удаления привычки.
+    """
+
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
