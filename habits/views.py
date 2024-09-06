b7e701c061647941f162bcc0becd96542e134080
Вячеслав Лукин <bafana.slv@gmail.com>
2024-09-06 21:20:10 +0300

    Курсовая работа DRF.

--- habits/views.py
+++ habits/views.py
@@ -63,7 +63,7 @@ class HabitUpdateAPIView(generics.UpdateAPIView):
     def perform_update(self, serializer):
         """Перед сохраннием привычки проверяем не ссылается связанная привычка на саму себя."""
         habit = serializer.save()
-        if not habit.is_nice and habit.related_habit.id and habit.id == habit.related_habit.id:
+        if not habit.is_nice and habit.related_habit and habit.id == habit.related_habit.id:
             raise ValidationError(
                 f"Связанная привычка не может быть ссылкой на саму себя !"
             )
